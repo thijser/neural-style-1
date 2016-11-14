76e219e0551abd0c0af6745259a8044fcb8c7148
thijser <thijsboumans94@gmail.com>
2016-11-14 10:13:02 +0100

    new file

---
+++ correctionWithStyle.lua
@@ -0,0 +1,89 @@
+require 'torch'
+require 'nn'
+require 'image'
+require 'optim'
+
+require 'loadcaffe'
+
+
+local cmd = torch.CmdLine()
+
+-- Basic options
+cmd:option('-style_image', 'examples/inputs/seated-nude.jpg',
+           'Style target image')
+cmd:option('-style_blend_weights', 'nil')
+cmd:option('-content_image', 'examples/inputs/tubingen.jpg',
+           'Content target image')
+cmd:option('-image_size', 512, 'Maximum height / width of generated image')
+cmd:option('-gpu', 0, 'Zero-indexed ID of the GPU to use; for CPU mode set -gpu = -1')
+
+-- Optimization options
+cmd:option('-content_weight', 5e0)
+cmd:option('-style_weight', 1e2)
+cmd:option('-tv_weight', 1e-3)
+cmd:option('-num_iterations', 1000)
+cmd:option('-normalize_gradients', false)
+cmd:option('-init', 'image', 'random|image')
+cmd:option('-optimizer', 'lbfgs', 'lbfgs|adam')
+cmd:option('-learning_rate', 1e1)
+
+-- Output options
+cmd:option('-print_iter', 50)
+cmd:option('-save_iter', 100)
+cmd:option('-output_image', 'out.png')
+
+-- Other options
+cmd:option('-style_scale', 1.0)
+cmd:option('-original_colors', 0)
+cmd:option('-pooling', 'max', 'max|avg')
+cmd:option('-proto_file', 'models/VGG_ILSVRC_19_layers_deploy.prototxt')
+cmd:option('-model_file', 'models/VGG_ILSVRC_19_layers.caffemodel')
+cmd:option('-backend', 'nn', 'nn|cudnn|clnn')
+cmd:option('-cudnn_autotune', false)
+cmd:option('-seed', -1)
+
+cmd:option('-content_layers', 'relu4_2', 'layers for content')
+cmd:option('-style_layers', 'relu1_1,relu2_1,relu3_1,relu4_1,relu5_1', 'layers for style')
+
+local function main(params)
+  if params.gpu >= 0 then
+    if params.backend ~= 'clnn' then
+      require 'cutorch'
+      require 'cunn'
+      cutorch.setDevice(params.gpu + 1)
+    else
+      require 'clnn'
+      require 'cltorch'
+      cltorch.setDevice(params.gpu + 1)
+    end
+  else
+    params.backend = 'nn'
+  end
+
+  if params.backend == 'cudnn' then
+    require 'cudnn'
+    if params.cudnn_autotune then
+      cudnn.benchmark = true
+    end
+    cudnn.SpatialConvolution.accGradParameters = nn.SpatialConvolutionMM.accGradParameters -- ie: nop
+  end
+  
+  local loadcaffe_backend = params.backend
+  if params.backend == 'clnn' then loadcaffe_backend = 'nn' end
+  local cnn = loadcaffe.load(params.proto_file, params.model_file, loadcaffe_backend):float()
+  if params.gpu >= 0 then
+    if params.backend ~= 'clnn' then
+      cnn:cuda()
+    else
+      cnn:cl()
+    end
+  end
+  local pre_image = image.load(params.pre_image, 3)
+  pre_image = image.scale(content_image, params.image_size, 'bilinear')
+  local pre_image_caffe = preprocess(content_image):float()
+
+end
+
+
+local params = cmd:parse(arg)
+main(params)
