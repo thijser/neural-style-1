d473a52cbe21b7abe936acca1104a4fbdeeb3dc5
thijser <thijsboumans94@gmail.com>
2016-11-15 14:39:15 +0100

    loading

---
+++ Correlations.lua
@@ -0,0 +1,192 @@
+require 'torch'
+require 'nn'
+require 'image'
+require 'optim'
+
+require 'loadcaffe'
+
+
+local cmd = torch.CmdLine()
+
+-- Basic options
+cmd:option('-pre_image', 'examples/inputs/seated-nude.jpg',
+           'Style target image')
+cmd:option('-style_blend_weights', 'nil')
+cmd:option('-post_image', 'examples/inputs/tubingen.jpg',
+           'Content target image')
+cmd:option('-image_size', 512, 'Maximum height / width of generated image')
+cmd:option('-gpu', 0, 'Zero-indexed ID of the GPU to use; for CPU mode set -gpu = -1')
+cmd:option('-edit_image', 'examples/inputs/seated-nude.jpg',
+           'Style target image')
+-- Optimization options
+cmd:option('-content_weight', 5e0)
+cmd:option('-style_weight', 1e2)
+cmd:option('-tv_weight', 1e-3)
+cmd:option('-num_iterations', 1000)
+cmd:option('-normalize_gradients', false)
+cmd:option('-init', 'image', 'random|image')
+cmd:option('-optimizer', 'lbfgs', 'lbfgs|adam')
+cmd:option('-learning_rate', 1e1)
+
+-- Output options
+cmd:option('-print_iter', 50)
+cmd:option('-save_iter', 100)
+cmd:option('-output_image', 'out.png')
+
+-- Other options
+cmd:option('-style_scale', 1.0)
+cmd:option('-original_colors', 0)
+cmd:option('-pooling', 'max', 'max|avg')
+cmd:option('-proto_file', 'models/VGG_ILSVRC_19_layers_deploy.prototxt')
+cmd:option('-model_file', 'models/VGG_ILSVRC_19_layers.caffemodel')
+cmd:option('-backend', 'nn', 'nn|cudnn|clnn')
+cmd:option('-cudnn_autotune', false)
+cmd:option('-seed', -1)
+
+cmd:option('-content_layers', 'relu4_2', 'layers for content')
+cmd:option('-style_layers', 'relu1_1,relu2_1,relu3_1,relu4_1,relu5_1', 'layers for style')
+
+local function main(params)
+  if params.gpu >= 0 then
+    if params.backend ~= 'clnn' then
+      require 'cutorch'
+      require 'cunn'
+      cutorch.setDevice(params.gpu + 1)
+    else
+      require 'clnn'
+      require 'cltorch'
+      cltorch.setDevice(params.gpu + 1)
+    end
+  else
+    params.backend = 'nn'
+  end
+
+  if params.backend == 'cudnn' then
+    require 'cudnn'
+    if params.cudnn_autotune then
+      cudnn.benchmark = true
+    end
+    cudnn.SpatialConvolution.accGradParameters = nn.SpatialConvolutionMM.accGradParameters -- ie: nop
+  end
+  
+  local loadcaffe_backend = params.backend
+  if params.backend == 'clnn' then loadcaffe_backend = 'nn' end
+  local cnn = loadcaffe.load(params.proto_file, params.model_file, loadcaffe_backend):float()
+  if params.gpu >= 0 then
+    if params.backend ~= 'clnn' then
+      cnn:cuda()
+    else
+      cnn:cl()
+    end
+  end
+  --load pre image 
+  local pre_image = image.load(params.pre_image, 3)
+  pre_image = image.scale(pre_image, params.image_size, 'bilinear')
+  local pre_image_caffe = preprocess(pre_image):float()
+  
+  --load post image 
+    local post_image = image.load(params.post_image, 3)
+  post_image = image.scale(post_image, params.image_size, 'bilinear')
+  local post_image_caffe = preprocess(post_image):float()
+  
+  if params.gpu >= 0 then
+    if params.backend ~= 'clnn' then
+      pre_image_caffe = pre_image_caffe:cuda()
+      post_image_caffe = post_image_caffe:cuda() 
+    else
+            pre_image_caffe = pre_image_caffe:cl()
+            post_image_caffe = post_image_caffe:cl()
+    end
+   end
+  
+
+  -- Set up the network, inserting style and content loss modules
+  local CorrectionLoss {}
+  local next_content_idx, next_style_idx = 1, 1
+  local net = nn.Sequential()
+  if params.tv_weight > 0 then
+    local tv_mod = nn.TVLoss(params.tv_weight):float()
+    if params.gpu >= 0 then
+      if params.backend ~= 'clnn' then
+        tv_mod:cuda()
+      else
+        tv_mod:cl()
+      end
+    end
+    net:add(tv_mod)
+  end
+
+  for i = 1, #cnn do
+  	
+        local layer = cnn:get(i)
+      local name = layer.name
+      local layer_type = torch.type(layer)
+      local is_pooling = (layer_type == 'cudnn.SpatialMaxPooling' or layer_type == 'nn.SpatialMaxPooling')
+      if is_pooling and params.pooling == 'avg' then
+        assert(layer.padW == 0 and layer.padH == 0)
+        local kW, kH = layer.kW, layer.kH
+        local dW, dH = layer.dW, layer.dH
+        local avg_pool_layer = nn.SpatialAveragePooling(kW, kH, dW, dH):float()
+        if params.gpu >= 0 then
+          if params.backend ~= 'clnn' then
+            avg_pool_layer:cuda()
+          else
+            avg_pool_layer:cl()
+          end
+        end
+        local msg = 'Replacing max pooling at layer %d with average pooling'
+        print(string.format(msg, i))
+        net:add(avg_pool_layer)
+      else
+        net:add(layer)
+      end
+  end
+  
+end
+
+
+--prepare image for caffe 
+function preprocess(img)
+  local mean_pixel = torch.DoubleTensor({103.939, 116.779, 123.68})
+  local perm = torch.LongTensor{3, 2, 1}
+  img = img:index(1, perm):mul(256.0)
+  mean_pixel = mean_pixel:view(3, 1, 1):expandAs(img)
+  img:add(-1, mean_pixel)
+  return img
+end
+
+local TVLoss, parent = torch.class('nn.TVLoss', 'nn.Module')
+
+function TVLoss:__init(strength)
+  parent.__init(self)
+  self.strength = strength
+  self.x_diff = torch.Tensor()
+  self.y_diff = torch.Tensor()
+end
+function TVLoss:updateOutput(input)
+  self.output = input
+  return self.output
+end
+
+-- TV loss backward pass inspired by kaishengtai/neuralart
+function TVLoss:updateGradInput(input, gradOutput)
+  self.gradInput:resizeAs(input):zero()
+  local C, H, W = input:size(1), input:size(2), input:size(3)
+  self.x_diff:resize(3, H - 1, W - 1)
+  self.y_diff:resize(3, H - 1, W - 1)
+  self.x_diff:copy(input[{{}, {1, -2}, {1, -2}}])
+  self.x_diff:add(-1, input[{{}, {1, -2}, {2, -1}}])
+  self.y_diff:copy(input[{{}, {1, -2}, {1, -2}}])
+  self.y_diff:add(-1, input[{{}, {2, -1}, {1, -2}}])
+  self.gradInput[{{}, {1, -2}, {1, -2}}]:add(self.x_diff):add(self.y_diff)
+  self.gradInput[{{}, {1, -2}, {2, -1}}]:add(-1, self.x_diff)
+  self.gradInput[{{}, {2, -1}, {1, -2}}]:add(-1, self.y_diff)
+  self.gradInput:mul(self.strength)
+  self.gradInput:add(gradOutput)
+  return self.gradInput
+end
+
+
+
+local params = cmd:parse(arg)
+main(params)
