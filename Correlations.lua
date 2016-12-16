35075d77687da26eb825e0eb500e39dcb9eee94a
thijser <thijsboumans94@gmail.com>
2016-12-16 10:47:59 +0100

    missing end

--- Correlations.lua
+++ Correlations.lua
@@ -140,7 +140,7 @@ local function main(params)
       else
         net:add(layer)
       end
-            if name == content_layers[next_content_idx] then
+      if name == content_layers[next_content_idx] then
         print("Setting up content layer", i, ":", layer.name)
         local target = net:forward(content_image_caffe):clone()
         local norm = params.normalize_gradients
@@ -152,6 +152,7 @@ local function main(params)
             loss_module:cl()
           end
         end
+      end  
   end
   
 end
