f12bec5e63c37bfa5ba133c175f18a0caad7ef8f
thijser <thijsboumans94@gmail.com>
2016-12-23 14:14:00 +0100

    typo

--- Correlations.lua
+++ Correlations.lua
@@ -155,7 +155,7 @@ local function main(params)
       		print("\n checking for relevant")
               print(name)
               print(next_content_idx)
-			  print(re_image[next_content_idx])
+			  print(pre_image[next_content_idx])
       if name == pre_image[next_content_idx] then
         print("Setting up content layer", i, ":", layer.name)
         local target = net:forward(content_image_caffe):clone()
