c6914845928bd5483f436d9feaa24b506503e649
thijser <thijsboumans94@gmail.com>
2017-01-13 09:13:57 +0100

    begining lin res

--- exp2.lua
+++ exp2.lua
@@ -371,15 +371,82 @@ local function main(params)
 end
   
 function correlate(preimage,postimage,actualImage)
-
-	target = preimage+postimage+actualImage
+	
+--	target = preimage+postimage+actualImage
 --	target= torch.cmul(torch.cdiv(postimage,preimage),actualImage)
 --	nan_mask = target:ne(target)
 --	target[nan_mask] = 1
 --	nan_mask=nil
 --	print(target[nan_mask])
 --	  collectgarbage()
-	return target
+
+--linear regression see https://github.com/torch/demos/blob/master/linear-regression/example-linear-regression.lua
+	local mod = nn.Sequential()
+	local ninputs = preimage.size(1); 
+	local noutputs = 1
+	local criterion = nn.MSECriterion()
+    mod:add(nn.Linear(ninputs, noutputs))
+    local x, dl_dx = model:getParameters()
+    
+    feval = function(x_new)
+   -- set x to x_new, if differnt
+   -- (in this simple example, x_new will typically always point to x,
+   -- so the copy is really useless)
+   if x ~= x_new then
+      x:copy(x_new)
+   end
+
+   -- select a new training sample
+   _nidx_ = (_nidx_ or 0) + 1
+   if _nidx_ > (#data)[1] then _nidx_ = 1 end
+
+   local sample = data[_nidx_]
+   local target = sample[{ {1} }]      -- this funny looking syntax allows
+   local inputs = sample[{ {2,3} }]    -- slicing of arrays.
+
+   -- reset gradients (gradients are always accumulated, to accommodate 
+   -- batch methods)
+   dl_dx:zero()
+
+   -- evaluate the loss function and its derivative wrt x, for that sample
+   local loss_x = criterion:forward(model:forward(inputs), target)
+   model:backward(inputs, criterion:backward(model.output, target))
+
+   -- return loss(x) and dloss/dx
+   return loss_x, dl_dx
+end
+
+
+for i = 1,1e4 do
+
+   -- this variable is used to estimate the average loss
+   current_loss = 0
+
+   -- an epoch is a full loop over our training data
+   for i = 1,(#data)[1] do
+
+      -- optim contains several optimization algorithms. 
+      -- All of these algorithms assume the same parameters:
+      --   + a closure that computes the loss, and its gradient wrt to x, 
+      --     given a point x
+      --   + a point x
+      --   + some parameters, which are algorithm-specific
+      
+      _,fs = optim.sgd(feval,x,sgd_params)
+
+      -- Functions in optim all return two things:
+      --   + the new x, found by the optimization method (here SGD)
+      --   + the value of the loss functions at all points that were used by
+      --     the algorithm. SGD only estimates the function once, so
+      --     that list just contains one value.
+
+      current_loss = current_loss + fs[1]
+   end
+
+end 
+  
+    
+	return current_loss
 end
 
 
